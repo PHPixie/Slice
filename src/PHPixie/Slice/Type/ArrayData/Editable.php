521a18ffea61f3f08f33e075ff87e75467c4051a
Linfuby <web_dev@parisclub.ru>
2016-01-19 17:23:58 +0400

    Append add for Editable

--- src/PHPixie/Slice/Type/ArrayData/Editable.php
+++ src/PHPixie/Slice/Type/ArrayData/Editable.php
@@ -24,6 +24,22 @@ class Editable extends    \PHPixie\Slice\Type\ArrayData
         $parent[$key] = $value;
     }
 
+    public function add($path, $value)
+    {
+        if ($path === null) {
+
+            if (!is_array($value)) {
+                throw new \PHPixie\Slice\Exception("Only array values can be set as root");
+            }
+            $this->data = array_replace_recursive($this->data, $value);
+            return;
+        }
+
+        list($path, $key) = $this->splitPath($path);
+        $parent = &$this->findGroup($path, true);
+        $parent[$key] = array_replace_recursive($parent[$key], $value);
+    }
+
     /**
      * @throws \PHPixie\Slice\Exception
      */
